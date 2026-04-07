6d57f73fcaf80a0e6771161e67bb721f9f2ee661
Biwjustwork <biwmlg43205@gmail.com>
2026-04-07 10:58:28 +0700

    commit1

---
+++ src/lab/MyStack.java
@@ -0,0 +1,27 @@
+package lab;
+
+public class MyStack {
+
+    private int top;
+    private int list;
+
+    public MyStack(int top) {
+
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+    }
+
+    public int getList() {
+        return list;
+    }
+
+    public void setList(int list) {
+        this.list = list;
+    }
+}
